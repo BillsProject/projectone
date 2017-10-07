ffa0ee6ca2eb0e2889d96587ef392c0d9de77c60
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-10-06 21:36:31 -0700

    added billUrl

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -55,6 +55,7 @@ function displayBill() {
 			var actionP = $('<p class="actions">').text("Latest Major Action: " + majorAction);
 			billDiv.append(actionP);
 			
+			var billUrl = response.results[0].bills[i].congressdotgov_url;
 			var learn = $('<button>').html($('<a href=' + billUrl + ' target="_blank">Learn More</a>'));
 			$(learn).attr("class", "btn").attr("type", "button");
 			billDiv.append(learn);
