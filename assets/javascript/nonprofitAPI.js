223ba7532adcacccb1d3ed1845021e07ff283445
dblanchard13 <davidb.prof@gmail.com>
2017-10-19 21:42:49 -0700

    Project comments.

--- assets/javascript/nonprofitAPI.js
+++ assets/javascript/nonprofitAPI.js
@@ -8,9 +8,9 @@ $(".item").click(function(displayOrgs) {
 	$(".item").select();
 	//split and join to take care of spaces
 	var state = ($(this).text()).split(' ').join('+');
-	console.log(state);
+	// console.log(state);
 	var newTopic = ($(".current").text()).split(' ').join('+');
-	console.log(newTopic);
+	// console.log(newTopic);
 
 	//empty orgs div to clear previous search results
 	$("#orgs").empty();
@@ -24,7 +24,7 @@ $(".item").click(function(displayOrgs) {
 	var csKey =  "007404496376415496285:znz6cfip7pc"
 	// url
 	var urlOrgs = "https://www.googleapis.com/customsearch/v1?key=" + orgsKey + "&cx=" + csKey + "&q=" + state + "+" + newTopic
-	console.log(urlOrgs);
+	// console.log(urlOrgs);
 
 	//start ajax request
 	$.ajax({
@@ -33,21 +33,24 @@ $(".item").click(function(displayOrgs) {
 		dataType: "json",
 		method: "GET"
 	}).done(function(response) {
-			console.log(response);
+			// console.log(response);
 
 			//begin for loop to stringify and append results to sidbar div
 			var responseItems = response.items;
-			console.log(responseItems);
+			// console.log(responseItems);
 
 			
 
 			for (var i = 0; i < responseItems.length; i++) {
 				//stringify link
+				// The link should already be a string so you don't need to stringify it
+				// and then you also wouldn't need to call the replace function on it
+				// just below.
 				var stringLink = JSON.stringify(responseItems[i].formattedUrl);
-				console.log(stringLink);
+				// console.log(stringLink);
 				//remove quotation marks from links
 				var getLink = stringLink.replace(/['"]+/g, '');
-				console.log(getLink);
+				// console.log(getLink);
 				//dynamically create div and append to sidebar
 				var linkDiv = $("<div class='link-container'>");
 				$("#orgs").append(linkDiv);
@@ -65,6 +68,7 @@ $(".item").click(function(displayOrgs) {
 				var snippetDiv = $("<div class='snippet-container'>");
 				$("#orgs").append(snippetDiv);
 				//stringify and append snippet
+				// Again, the snippet should already be a string so this stringify call is unnecessary.
 				var snippet = JSON.stringify(responseItems[i].snippet);
 				$(".snippet-container").append(snippet);
 
